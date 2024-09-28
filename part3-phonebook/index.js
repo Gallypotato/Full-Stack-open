871c8e8e4d7e73dfd4f8e34ece41fa388d292afb
Gally <purevery-g@live.com>
2024-09-28 00:05:03 -0400

    error handling into middleware and some others operations

--- part3-phonebook/index.js
+++ part3-phonebook/index.js
@@ -4,35 +4,62 @@ const morgan = require('morgan')
 const app = express()
 const PORT = process.env.PORT || 3001
 const cors = require('cors')
-const Person  = require('./models/person')
+const Person  = require('./models/person');
+
+app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+//3.16
+//error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
 
 app.use(cors())
 app.use(express.static('build'))
-let persons = [
-    { 
-        id: 1,
-        name: "Arto Hellas", 
-        number: "040-123456"
-      },
-      { 
-        id: 2,
-        name: "Ada Lovelace", 
-        number: "39-44-5323523"
-      },
-      { 
-        id: 3,
-        name: "Dan Abramov", 
-        number: "12-43-234345"
-      },
-      { 
-        id: 4,
-        name: "Mary Poppendieck", 
-        number: "39-23-6423122"
-      }
-]
+//let persons = [
+//    { 
+//        id: 1,
+//        name: "Arto Hellas", 
+//        number: "040-123456"
+//      },
+//      { 
+//        id: 2,
+//        name: "Ada Lovelace", 
+//        number: "39-44-5323523"
+//      },
+//      { 
+//        id: 3,
+//        name: "Dan Abramov", 
+//        number: "12-43-234345"
+//      },
+//      { 
+//        id: 4,
+//        name: "Mary Poppendieck", 
+//        number: "39-23-6423122"
+//      }
+//]
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 //3.8
 morgan.token('body', (req) => {
@@ -62,22 +89,24 @@ app.post('/info',(request,response) => {
 })
 //3.3
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-      console.log('x')
-      response.status(404).end()
-    }
+    Person.findById(request.params.id).then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        console.log('x')
+        response.status(404).end()
+      }
+    })    
+    .catch(error => next(error))
   })
+  
 
 //3.4
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
-})
+//app.delete('/api/persons/:id', (request, response) => {
+//  const id = Number(request.params.id)
+//  persons = persons.filter(person => person.id !== id)
+//  response.status(204).end()
+//})
 
 //3.5
 const generateId = () => {
@@ -123,7 +152,36 @@ app.post('/api/persons',(request, response) =>{
     })
 })
 
+//3.15
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
+//3.17
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number || false,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
